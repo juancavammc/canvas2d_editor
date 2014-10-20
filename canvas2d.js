9df5bf7b35219277b8b0f5d52062e99f8a7497c5
Ricardo Navarro <rcnavarro7@gmail.com>
2014-10-20 20:03:22 +0200

    .

---
+++ canvas2d.js
@@ -0,0 +1,10 @@
+'use strict';
+function init() {
+  var ctx = document.getElementById('canvas_test').getContext('2d');
+
+  ctx.fillStyle = "rgb(200,0,0)";
+  ctx.fillRect (10, 10, 55, 50);
+
+  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+  ctx.fillRect (30, 30, 55, 50);
+}
