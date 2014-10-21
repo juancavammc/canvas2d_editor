538a87ab11e243785998fae3d35d173bd7c87be2
Ricardo Navarro <rcnavarro7@gmail.com>
2014-10-21 16:04:22 +0200

    Drag & Drop de imágenes.

--- canvas2d.js
+++ canvas2d.js
@@ -1,10 +1,41 @@
 'use strict';
 function init() {
-  var ctx = document.getElementById('canvas_test').getContext('2d');
+    var ctx = document.getElementById("canvas_test").getContext("2d");
+    var drop_zone = document.getElementById("drop_zone");
+    ctx.canvas.width = drop_zone.offsetWidth;
+    ctx.canvas.height = drop_zone.offsetHeight;
+    console.log(drop_zone.offsetWidth);
+    console.log(drop_zone.offsetHeight);
+    
+    function handle_dragover(event) {
+        console.log("dragover");
+        event.stopPropagation();
+        event.preventDefault();
+    }
+    
+    function handle_drop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        var files = event.dataTransfer.files;
+        if(files.length) {
+            var file = files[0];
+            var reader = new FileReader();
+            reader.onloadend = function() { 
+                console.log(this.result);
+                var img = new Image();
+                img.src = this.result;
+                img.onload = function() {
+                    ctx.drawImage(this, 0, 0);
+                }
+            };
+            reader.readAsDataURL(file);
+            console.log(reader.result);
+        }
+        console.log("drop");
+    }
+    
+    drop_zone.addEventListener("dragover", handle_dragover, false);
+    drop_zone.addEventListener("drop", handle_drop, false);
+    
 
-  ctx.fillStyle = "rgb(200,0,0)";
-  ctx.fillRect (10, 10, 55, 50);
-
-  ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-  ctx.fillRect (30, 30, 55, 50);
-}
+};
